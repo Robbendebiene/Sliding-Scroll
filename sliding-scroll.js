caa8f4a48826facdb144fd9fb4e0bc2ec73ced48
章鱼哥 <s969980719@vip.qq.com>
2022-03-11 02:16:17 +0800

    Add custom offset parameter (#1)

--- sliding-scroll.js
+++ sliding-scroll.js
@@ -35,18 +35,20 @@ function scrollToTop(duration = 0) {
  * id: the id of the element as a string that should be scrolled to
  * duration: scroll duration in milliseconds; default is 0 (no transition)
  * this function is using the scrollToY function on the main scrolling element
+ * customOffset: an additional offset value to adjust the scroll position; default is 0 (no additional offset)
  */
-function scrollToId(id, duration = 0) {
+function scrollToId(id, duration = 0, customOffset = 0) {
   const offset = Math.round(document.getElementById(id).getBoundingClientRect().top);
-	scrollToY(document.scrollingElement.scrollTop + offset, duration);
+	scrollToY(document.scrollingElement.scrollTop + offset + customOffset, duration);
 }
 
 /*
  * element: the node object that should be scrolled to
  * duration: scroll duration in milliseconds; default is 0 (no transition)
  * this function is using the scrollToY function on the main scrolling element
+ * customOffset: an additional offset value to adjust the scroll position; default is 0 (no additional offset)
  */
-function scrollToElement(element, duration = 0) {
+function scrollToElement(element, duration = 0, customOffset = 0) {
 	const offset = Math.round(element.getBoundingClientRect().top);
-	scrollToY(document.scrollingElement.scrollTop + offset, duration);
+	scrollToY(document.scrollingElement.scrollTop + offset + customOffset, duration);
 }
